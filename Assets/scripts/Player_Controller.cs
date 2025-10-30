439a337357a5cf1e1683d106baa0c9dcfd50342d
ImagineBeingOnline <sebastian.marin@discoverystudent.org>
2025-10-30 11:42:13 -0400

    something

---
+++ Assets/scripts/Player_Controller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine. InputSystem;
+
+public class Player_Controller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Voin OnMove(InputValue movementValue)
+}
